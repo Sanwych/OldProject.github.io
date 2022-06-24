0795d99799d3acb930243937e5f6c86ab1360bdf
Sanwych <106179337+Sanwych@users.noreply.github.com>
2022-06-24 21:23:12 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,13 +1,17 @@
 const boton = document.getElementById('menu')
 const sidebar = document.getElementById('sidebar')
+const mapa = document.getElementById('main')
 boton.addEventListener('click', () => {
 
     if(sidebar.classList.contains('on')){
         sidebar.classList.add("off")
         sidebar.classList.remove("on")
+        mapa.style.opacity ="100%"
+        
     } else if(sidebar.classList.contains('off')){
         sidebar.classList.add("on")
         sidebar.classList.remove("off")
+        mapa.style.opacity ="40%"
     }
  
 })
