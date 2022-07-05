dd090d3a6e905c189e5d48d9bf04ff05760b40e6
Sanwych <106179337+Sanwych@users.noreply.github.com>
2022-07-05 16:38:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const context = canvas.getContext('2d')
 canvas.width = 1600
 canvas.height = 900
 
-const image = new Image()
-image.src ="./img/mapaeste.png"
+const imagen = new Image()
+imagen.src ="./img/mapaeste.png"
 
 const player = new Image()
 player.src = "./img/playerDown.png"
@@ -39,7 +39,7 @@ class Boundary {
     }
 
     draw(){
-        context.fillStyle = 'red'
+        context.fillStyle = ' rgba(255,0,0,0) '
         context.fillRect(this.position.x,this.position.y, this.width, this.height)  
     }   
 }
@@ -60,24 +60,51 @@ collisionMap.forEach((row, i) => {
 ))}})})
 
 class Propiedades{
-    constructor({position, speed, image}){
+    
+    constructor({position, width, height, image, frames = {default: 1}}){
         this.position = position
-        this.speed = speed
-        this.image = image    
+        this.image = image   
+        this.frames = frames 
+        this.width = width
+        this.height = height
+    
     }
      draw(){
-        context.drawImage(this.image, this.position.x , this.position.y) 
+        context.drawImage(this.image, 
+  
+            0,
+            0,
+            this.image.width / this.frames.default , 
+            this.image.height,  
+            this.position.x,
+            this.position.y, 
+            this.image.width / this.frames.default , 
+            this.image.height
+            ) 
        
     }
 }
+
+const mainChar = new Propiedades({
+    position:{
+        x:canvas.width / 2 - 192 / 3, 
+        y:canvas.height / 2 - 68 / 2,
+    }, 
+    image:player,
+    frames:{default:4},
+    width: 160 / 4,
+    height: 68
+     
+    
+})
 
 const background = new Propiedades({
     position:{
     x: offsetX,
     y: offsetY
     },
-    speed: "speed",
-    image:image
+    
+    image:imagen
     })
 
     const keys = {   
@@ -95,36 +122,64 @@ const background = new Propiedades({
         },
        keystate: {
         lastUpped: "",
-        lastKey: "",
+        lastKey: ""
         }
     }
 
     const movableItem = [background, ...boundaries]
+
+    function stopPlayer({e1,e2}){
+        
+         return   (e1.position.x + e1.width >= e2.position.x &&
+                  e1.position.x <= e2.position.x + e2.width &&
+                  e1.position.y <= e2.position.y + e2.height &&
+                  e1.position.y + e1.height >= e2.position.y)   
+    }
+
     
-
-
-
-    
+  
 
 function animation(){
-
+    
     window.requestAnimationFrame(animation) 
+    
     background.draw()
+    
     boundaries.forEach(boundary => {
-        boundary.draw()
+        boundary.draw()    
+    
     })
-    context.drawImage(player, 
-  
-        0,0,player.width / 4 , 
-        player.height,   
-        canvas.width / 2 - player.width / 3, 
-        canvas.height / 2,
-        player.width / 4 , 
-        player.height,)  
-      
+
+    mainChar.draw()
+    
   if(sidebar.classList.contains('off')){
 
+    
+    
+    let moving = true
+    
     if(keys.w.pressed == true && keys.keystate.lastKey === 'w'){
+      for(let i = 0; i < boundaries.length; i++){
+         const boundary = boundaries[i]
+          if(
+            stopPlayer({
+             e1:mainChar,
+             e2:{
+                ...boundary
+                ,position:{
+               x: boundary.position.x,
+               y: boundary.position.y + 3,
+             }}
+            }
+            )){
+                moving = false 
+                
+                console.log(moving)
+                break
+            }
+      }
+        
+        if(moving == true)
         movableItem.forEach((movableItem) =>{
             movableItem.position.y += 3
 
@@ -132,6 +187,24 @@ function animation(){
         
       }
        else if (keys.a.pressed == true && keys.keystate.lastKey === 'a'){
+        for(let i = 0; i<boundaries.length;i++){
+            const boundary = boundaries[i]
+             if(stopPlayer({
+                e1:mainChar,
+                e2:{...boundary,position:{
+                  x: boundary.position.x + 3,
+                  y: boundary.position.y
+                }
+               }}
+               )){
+                
+                   moving = false
+                   console.log(moving)
+                   break
+               }
+         }
+           
+           if(moving == true)
         movableItem.forEach((movableItem) =>{
             movableItem.position.x += 3
         })
@@ -139,12 +212,48 @@ function animation(){
         
       }
        else if (keys.s.pressed == true && keys.keystate.lastKey === 's'){
+        for(let i = 0; i<boundaries.length;i++){
+            const boundary = boundaries[i]
+             if(stopPlayer({
+                e1:mainChar,
+                e2:{...boundary,position:{
+                  x: boundary.position.x,
+                  y: boundary.position.y - 3,
+                }}
+               }
+               )){
+                
+                   moving = false
+                   console.log(moving)
+                   break
+               }
+         }
+           
+           if(moving == true)
         movableItem.forEach((movableItem) =>{
             movableItem.position.y -= 3
         })
          
       }
        else if (keys.d.pressed == true && keys.keystate.lastKey === 'd'){
+        for(let i = 0; i<boundaries.length;i++){
+         const boundary = boundaries[i]
+          if(stopPlayer({
+             e1:mainChar,
+             e2:{...boundary,position:{
+               x: boundary.position.x - 3,
+               y: boundary.position.y
+             }}
+            }
+            )){
+                
+                moving = false
+                console.log(moving)
+                break
+            }
+      }
+        
+        if(moving == true)
         movableItem.forEach((movableItem) =>{
             movableItem.position.x -= 3
         })  
@@ -161,7 +270,6 @@ function animation(){
 
 
 animation()
-
 
 
  
