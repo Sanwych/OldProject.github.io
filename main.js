dd997aad8651c849d4bf0020b60484fd590a5e4d
Sanwych <106179337+Sanwych@users.noreply.github.com>
2022-07-08 20:18:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,38 @@ context.fillRect(0,0,canvas.width, canvas.height)
 const offsetX = -1185;
 const offsetY= -500
 
+const mainChar = new Propiedades({
+    position:{
+        x:canvas.width / 2 - 192 / 3, 
+        y:canvas.height / 2 - 68 / 2,
+    }, 
+    image:player,
+    frames:{default:4},
+    width: 160 / 4,
+    height: 63
+     
+    
+})
+
+const background = new Propiedades({
+    position:{
+    x: offsetX,
+    y: offsetY
+    },
+    
+    image:imagen
+    })
+
+
+const foreground = new Propiedades({
+    position:{
+        x: offsetX,
+        y: offsetY
+        },
+        
+        image:foregroundImg
+        })
+
 
 var collisionMap = []
 
@@ -45,31 +77,6 @@ collisionMap.forEach((row, i) => {
 
 ))}})})
 
-class Propiedades{
-    
-    constructor({position, width, height, image, frames = {default: 1}}){
-        this.position = position
-        this.image = image   
-        this.frames = frames 
-        this.width = width
-        this.height = height
-    
-    }
-     draw(){
-        context.drawImage(this.image, 
-  
-            0,
-            0,
-            this.image.width / this.frames.default , 
-            this.image.height,  
-            this.position.x,
-            this.position.y, 
-            this.image.width / this.frames.default , 
-            this.image.height
-            ) 
-       
-    }
-}
 
 
     const keys = {   
@@ -243,8 +250,8 @@ function animation(){
 animation()
 
 
-console.log(mainChar.width)
 
+ 
 
  
 
