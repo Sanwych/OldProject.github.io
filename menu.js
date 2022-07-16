0dbf0e1e3d56b9a05add954f9d17b6de6e5880dd
Sanwych <106179337+Sanwych@users.noreply.github.com>
2022-07-16 16:16:42 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,15 +3,19 @@ const sidebar = document.getElementById('sidebar')
 const mapa = document.getElementById('main')
 boton.addEventListener('click', () => {
 
-    if(sidebar.classList.contains('on')){
-        sidebar.classList.add("off")
-        sidebar.classList.remove("on")
-        mapa.style.opacity ="100%"
-        
-    } else if(sidebar.classList.contains('off')){
+    if(sidebar.classList.contains('off')){
         sidebar.classList.add("on")
         sidebar.classList.remove("off")
         mapa.style.opacity ="40%"
+        
+        
+        
     }
- 
+    else if(sidebar.classList.contains('on')){
+        sidebar.classList.add("off")
+        sidebar.classList.remove("on")
+        mapa.style.opacity ="100%"
+             
+    }
 })
+
